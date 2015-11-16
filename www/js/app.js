728e2e9ea2313dcac47b7fc166068d5d60462334
Enol Iglesias <enoliglesias@gmail.com>
2015-11-16 22:55:47 +0100

    Change app.js to print recipes on load

--- www/js/app.js
+++ www/js/app.js
@@ -4,12 +4,12 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     // HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     RecipeListView.prototype.template =  Handlebars.compile($("#recipe-list-tpl").html());
-    // RecipeView.prototype.template = Handlebars.compile($("#recipe-tpl").html());
+    RecipeView.prototype.template = Handlebars.compile($("#recipe-tpl").html());
 
     var service = new RecipeService();
     service.initialize().done(function () {
       router.addRoute('', function() {
-          $('#content').html(new RecipeListView(service.getRecipes()).render().$el);
+          service.printRecipes();
       });
 
       router.addRoute('recipes/:id', function(id) {
