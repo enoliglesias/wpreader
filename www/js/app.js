04649c101a33074c7745c867aab8eacc322a057a
Enol Iglesias <enoliglesias@gmail.com>
2015-11-19 23:53:11 +0100

    Add handlebars helper in order to show recipe image

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,10 @@
     RecipeListView.prototype.template =  Handlebars.compile($("#recipe-list-tpl").html());
     RecipeView.prototype.template = Handlebars.compile($("#recipe-tpl").html());
 
+    Handlebars.registerHelper('recipe_image', function(image_id) {
+      return _.find(recipe_images, function(recipe){ return recipe.id == image_id; }).source_url;
+    });
+
     var service = new RecipeService();
     service.initialize().done(function () {
       router.addRoute('', function() {
