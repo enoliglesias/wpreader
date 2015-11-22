0e4e9fd2c970767887ab51b5b2a46fc2e176efd8
Enol Iglesias <enoliglesias@gmail.com>
2015-11-22 01:57:17 +0100

    Add some dummy memory func to favs

--- www/lib/bvb.js
+++ www/lib/bvb.js
@@ -11,9 +11,18 @@ var BVB = (function () {
     }
 
     function getFavSpan(recipe_id){
-        var action = $("span[data-recipe-id=" + recipe_id + "]").data("action")
-        var fav_action = (action === "fav") ? "unfav" : "fav";
-        var fav_image = (action === "fav") ? "img/fav.png" : "img/unfav.png";
+        var actual_action = $("span[data-recipe-id=" + recipe_id + "]").data("action");
+        var fav_action = null,
+            fav_image = null;
+        if (actual_action === "fav"){
+            fav_action = "unfav";
+            fav_image = "img/fav.png"
+            favs.push(recipe_id);
+        }else{
+            fav_action = "fav";
+            fav_image = "img/unfav.png"
+            favs.splice(favs.indexOf(recipe_id), 1)
+        }
 
         return '<span class="fav-star" data-action="' + fav_action
       +'" data-recipe-id="' + recipe_id + '"><img id="fav-star-img" src="' + fav_image + '"/></span>'
