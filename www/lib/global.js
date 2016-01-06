c3edf052efb8ab59f056a770caf562314c1b6fb3
Enol Iglesias <enoliglesias@gmail.com>
2016-01-06 22:35:33 +0100

    Apply linter in global.js

--- www/lib/global.js
+++ www/lib/global.js
@@ -5,8 +5,8 @@ var interval_ready_id = null;
 var registered = false;
 
 Storage.prototype.setObj = function(key, obj) {
-  return this.setItem(key, JSON.stringify(obj))
+  return this.setItem(key, JSON.stringify(obj));
 }
 Storage.prototype.getObj = function(key) {
-  return JSON.parse(this.getItem(key))
+  return JSON.parse(this.getItem(key));
 }
