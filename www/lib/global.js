397f0bd63e38b5d929d64bbdbf3fc98619091881
Enol Iglesias <enoliglesias@gmail.com>
2016-01-18 21:34:36 +0100

    Add menu points to global vars

--- www/lib/global.js
+++ www/lib/global.js
@@ -5,6 +5,7 @@ var interval_ready_id = null;
 var registered = false;
 var last_updated = "1984-01-01";
 var application_started = false;
+var menu_options = ["home", "search", "favs", "about-me", "contact"];
 
 Storage.prototype.setObj = function(key, obj) {
   return this.setItem(key, JSON.stringify(obj));
