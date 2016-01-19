f8ca78925f6c08344779585f6065e9a0d8a57583
Enol Iglesias <enoliglesias@gmail.com>
2016-01-19 20:49:30 +0100

    Update settings sample to add register_device var

--- www/lib/settings.sample.js
+++ www/lib/settings.sample.js
@@ -2,10 +2,11 @@ var Settings = {
   pushificator_endpoint: "modify this",
   pushificator_token: "modify this",
   send_email_endpoint: "modify this",
-  send_email_password: "modify this"
+  send_email_password: "modify this",
   fav_star_base64: "modify this",
   unfav_star_base64: "modify this",
   recipes_in_home: 10,
   wp_posts_endpoint: "modify this",
-  wp_images_endpoint: "modify this"
+  wp_images_endpoint: "modify this",
+  register_devices: false
 };
